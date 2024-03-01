0504aaab623ab12f2953a6b29701ad749c0cc9a8
Leonid <vama.ru472159472159@gmail.com>
2024-03-01 16:01:06 +0700

    Add helper for getting mod method

---
+++ RTLC/Helpers/ModHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+namespace RTLC.Helpers;
+internal static class ModHelper
+{
+    private static MethodInfo? s_MethodToPatch;
+
+    public static bool TryFindMethod(string modId, string type, string method)
+    {
+        if (!BepInEx.Bootstrap.Chainloader.PluginInfos.TryGetValue(modId, out var pluginInfo))
+        {
+            return false;
+        }
+
+        var assembly = pluginInfo.Instance.GetType().Assembly;
+        var patchingType = assembly.GetType(type, false);
+
+        s_MethodToPatch = AccessTools.Method(patchingType, method);
+        return s_MethodToPatch != null;
+    }
+
+    public static MethodBase GetMethod() => s_MethodToPatch!;
+}
--- RTLC/Mods/ShipLoot/Patch_ShipLoot_OnScan.cs
+++ RTLC/Mods/ShipLoot/Patch_ShipLoot_OnScan.cs
@@ -1,34 +1,19 @@
 ﻿using System.Collections.Generic;
 using System.Reflection;
 using HarmonyLib;
+using RTLC.Helpers;
 using RTLC.Translations;
 
 namespace RTLC.Mods.ShipLoot;
 [HarmonyPatch]
 internal class Patch_ShipLoot_OnScan
 {
-    private static MethodInfo? s_MethodToPatch;
-
     [HarmonyPrepare]
     public static bool CheckModLoaded()
-    {
-        if (!BepInEx.Bootstrap.Chainloader.PluginInfos.TryGetValue(KnownPluginDependency.c_ShipLoot, out var pluginInfo))
-        {
-            return false;
-        }
-
-        var assembly = pluginInfo.Instance.GetType().Assembly;
-        var patchingType = assembly.GetType("ShipLoot.Patches.HudManagerPatcher", false);
-
-        s_MethodToPatch = AccessTools.Method(patchingType, "OnScan");
-        return s_MethodToPatch != null;
-    }
+        => ModHelper.TryFindMethod(KnownPluginDependency.c_ShipLoot, "ShipLoot.Patches.HudManagerPatcher", "OnScan");
 
     [HarmonyTargetMethod]
-    public static MethodBase GetMethodPatch()
-    {
-        return s_MethodToPatch!;
-    }
+    public static MethodBase GetMethodPatch() => ModHelper.GetMethod();
 
     [HarmonyTranspiler]
     public static IEnumerable<CodeInstruction> ReplaceString(IEnumerable<CodeInstruction> instructions)
